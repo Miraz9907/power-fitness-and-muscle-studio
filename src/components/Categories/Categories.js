221715e64938eb843e3802c0d3313ade1eb22031
Miraz9907 <meraz.rahman99@gmail.com>
2022-09-29 04:33:04 +0600

    information about user added in the cart

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
 import Category from "../Category/Category";
+import UserInfo from "../UserInfo/UserInfo";
 import "./Categories.css";
 
 const Categories = () => {
   const [categories, setCategories] = useState([]);
+//   const [requiredTime, setRequiredTime] = useState([]);
+
 
   useEffect(() => {
     fetch("data.json")
@@ -11,6 +14,12 @@ const Categories = () => {
       .then((data) => setCategories(data));
   }, []);
   // console.log(categories);
+
+//   const handleAddToList = (category) =>{
+//     console.log(category);
+//     const newList = [...requiredTime, category];
+//     setRequiredTime(newList);
+//   }
 
   return (
     <div className="container ">
@@ -20,13 +29,19 @@ const Categories = () => {
             <h2>Select Your today's GYM activities</h2>
             <div className="row g-3 mt-2">
               {categories.map((category) => (
-                <Category key={category.id} category={category}></Category>
+                <Category key={category.id} category={category} 
+                // handleAddToList = {handleAddToList}
+                >
+
+                </Category>
               ))}
             </div>
           </div>
         </div>
         <div className="col-3 border gx-3 ">
-          <h1>Right-part</h1>
+          <UserInfo 
+        //   requiredTime ={requiredTime}
+          ></UserInfo>
         </div>
       </div>
     </div>
--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -1,12 +1,62 @@
-import React from 'react';
-import './UserInfo.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+import logo from '../../Images/info.jpg';
+import "./UserInfo.css";
 
-const UserInfo = () => {
-    return (
+const UserInfo = (props) => {
+   console.log(props);
+//   const { requiredTime } = props;
+//   let totalTime = 0;
+//   for (const exercise of requiredTime) {
+//     totalTime = totalTime + exercise.time;
+//     console.log(totalTime);
+//   }
+//   const a =10;
+  return (
+    <div>
+      <div className="d-flex align-items-center justify-content-between p-0">
         <div>
-            
+          <img src={logo} alt=""  className=" rounded-circle info-img" />
         </div>
-    );
+        <div >
+          <h5 >Md Merazur Rahman</h5>
+          <div className="d-flex align-items-center justify-content-start">
+          <FontAwesomeIcon icon={faLocationDot}></FontAwesomeIcon>
+          <p className="">Dhaka, Bangladesh</p>
+          </div>
+          
+        </div>
+      </div>
+
+      <div className="bg-light my-2 d-flex align-items-center justify-content-evenly rounded-3 p-1">
+        <div>
+          <h4>
+            72<small className="fs-6">kg</small>
+          </h4>
+          <p>Weight</p>
+        </div>
+        <div>
+          <h4>5.8</h4>
+          <p>Height</p>
+        </div>
+        <div>
+          <h4>
+            22<small className="fs-6">yrs</small>
+          </h4>
+          <p>Age</p>
+        </div>
+      </div>
+      
+
+
+
+      
+
+      {/* <h3>Exercise</h3>
+      <h5>time: {totalTime}</h5> */}
+    </div>
+  );
 };
 
 export default UserInfo;
