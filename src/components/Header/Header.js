b61a8060c6f03db403818f7462b304795a42e57c
Miraz9907 <meraz.rahman99@gmail.com>
2022-09-28 23:39:18 +0600

    logo and name added in the main page

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from '../../Images/gym.png'
+
+const Header = () => {
+    return (
+        <div>
+            <div className='d-flex align-items-center bg-light ps-4 pt-4'>
+                <img src={logo} alt="" height="60" width="70" className='logo-img' />
+                <h2 className='mt-2 text-primary'>Power Fitness and Muscle Studio</h2>
+                
+            </div>
+        </div>
+    );
+};
+
+export default Header;
