1d1c101879e7c2c53a56e1e17a392057984dbd23
Miraz9907 <meraz.rahman99@gmail.com>
2022-09-29 01:45:54 +0600

    data show in the card dynamically

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,43 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import Category from '../Category/Category';
-import './Categories.css'
+import React, { useEffect, useState } from "react";
+import Category from "../Category/Category";
+import "./Categories.css";
 
 const Categories = () => {
-    const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState([]);
 
-    useEffect( () =>{
-        fetch('data.json')
-        .then(res => res.json())
-        .then(data => setCategories(data))
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
+  // console.log(categories);
 
-    },[])
-    // console.log(categories);
-
-    return (
-        <div className='container '>
-            
-
-            <div className='row gx-3 '>
-
-                <div className='col-9 border '>
-                <h2>Select Your today's GYM activities</h2>
-                <div className='row g-3'>
-                   {
-                        categories.map(category => <Category
-                        key = {category.id}
-                        category = {category}
-                        ></Category>)
-                    }
-
-                </div>
-                    
-                </div>
-                <div className='col-3 border gx-3'>
-                    <h1>Right-part</h1>
-                </div>
+  return (
+    <div className="container ">
+      <div className="row gx-5">
+        <div className="col-9 ">
+          <div className="mt-4">
+            <h2>Select Your today's GYM activities</h2>
+            <div className="row g-3 mt-2">
+              {categories.map((category) => (
+                <Category key={category.id} category={category}></Category>
+              ))}
             </div>
+          </div>
         </div>
-    );
+        <div className="col-3 border gx-3 ">
+          <h1>Right-part</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Categories;
--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import './Category.css';
 
 const Category = (props) => {
-    // const {name, picture, age, time, about} = props.category
+    const {name, picture, age, time, about} = props.category
 
     return (
 
         <div className='col-4'>
-            
+            <div className='card '>
+            <img src={picture} className=' rounded-4 card-img-top p-2 card-image' alt="..."/>
+            <div className='card-body'>
+                <h5 className='card-title'>{name}</h5>
+                <p className='card-text'>{about.slice(0,120) + '...'}</p>
+                <h6 className='card-title'>For Age :{age}</h6>
+                <h6 className='card-title'>Time required :{time}</h6>
+                <a href="/" class="btn btn-primary w-100">Add to list</a>
+            </div>
+            </div>
         </div>
     );
 };
