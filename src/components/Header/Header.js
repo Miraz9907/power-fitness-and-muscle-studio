d55376b317cd3671e14b01094764af9ed1dfa3a8
Miraz9907 <meraz.rahman99@gmail.com>
2022-09-29 16:36:57 +0600

    resize site name

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
         <div>
             <div className='d-flex align-items-center bg-light ps-4 pt-4'>
                 <img src={logo} alt=""  className='logo-img' />
-                <h2 className='mt-2 text-info'>Power Fitness and Muscle Studio</h2>
+                <h2 className='mt-2 text-info fst-italic fs-3'>Power Fitness and Muscle Studio</h2>
                 
             </div>
         </div>
